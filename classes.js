f7c9439839a08c5695d928371f16c3b22bc1fb95
Tristan Alie <TristanAlie@yahoo.com>
2023-03-07 19:39:02 -0500

    got one sprite image to work and centered the image so the circle is at the bottom of the image

--- classes.js
+++ classes.js
@@ -49,9 +49,17 @@ class Obstacle {
         this.game = game
         this.collisionX = Math.random() * this.game.width
         this.collisionY = Math.random() * this.game.height
-        this.collisionRadius = 50
+        this.collisionRadius = 60
+        this.image = document.getElementById('obstacles')
+        this.spriteWidth = 250
+        this.spriteHeight = 250
+        this.width = this.spriteWidth
+        this.height = this.spriteHeight
+        this.spriteX = this.collisionX - this.width / 2
+        this.spriteY = this.collisionY - this.height / 2 - 70
     }
     draw(context) {
+        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height)
         context.beginPath()
         context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
         // .save() and .restore() are if i want to change a state's settings for what's between them and have it not affect the rest of the code
@@ -69,7 +77,7 @@ class Game {
         this.width = this.canvas.width
         this.height = this.canvas.height
         this.player = new Player(this) // instantiating the player, later might be useful to put somewhere else instead/in addition to
-        this.numberOfObstacles = Math.ceil(Math.random() * 6)
+        this.numberOfObstacles = 10 // Math.ceil(Math.random() * 6)
         this.obstacles = []
         this.mouse = {
             x: this.canvas.width * 0.5,
@@ -78,7 +86,6 @@ class Game {
         }
         // the overlay is changed in the css file to make these work more reliably
         canvas.addEventListener('mousedown', (e) => { 
-            console.log(this.player)
             this.mouse.x = e.offsetX
             this.mouse.y = e.offsetY
             this.mouse.isPressed = true
@@ -101,8 +108,26 @@ class Game {
         this.obstacles.forEach(obstacle => obstacle.draw(context))
     }
     init() {
-        for(let i = 0; i < this.numberOfObstacles; i++) {
-            this.obstacles.push(new Obstacle(this))
+        // for(let i = 0; i < this.numberOfObstacles; i++) {
+        //     this.obstacles.push(new Obstacle(this))
+        // }
+        let attempts = 0
+        while(this.obstacles.length < this.numberOfObstacles && attempts < 200) {
+            let testObstacle = new Obstacle(this)
+            let overlap = false
+            this.obstacles.forEach(obstacle => {
+                const dx = testObstacle.collisionX - obstacle.collisionX
+                const dy = testObstacle.collisionY - obstacle.collisionY
+                const distance = Math.hypot(dy, dx)
+                const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius
+                if(distance < sumOfRadii) {
+                    overlap = true
+                }
+            })
+            if(!overlap) {
+                this.obstacles.push(testObstacle)
+            }
+            attempts++
         }
     }
 }
