551691e7290750de8c5678b6096cd1c771a060c1
Tristan Alie <TristanAlie@yahoo.com>
2023-03-08 15:43:54 -0500

    added the collision check

--- classes.js
+++ classes.js
@@ -41,6 +41,13 @@ class Player {
 
         this.collisionX += this.speedX * this.speedModifier
         this.collisionY += this.speedY * this.speedModifier
+        //collisions with obstacles
+        this.game.obstacles.forEach(obstacle => {
+            if(this.game.checkCollision(this, obstacle)) {
+                console.log('collision')
+            }
+            
+        })
     }
 }
 
@@ -109,6 +116,13 @@ class Game {
         this.player.draw(context)
         this.player.update()
         this.obstacles.forEach(obstacle => obstacle.draw(context))
+    }
+    checkCollision(a, b) {
+        const dx = a.collisionX - b.collisionX
+        const dy = a.collisionY - b.collisionY
+        const distance = Math.hypot(dy, dx)
+        const sumOfRadii = a.collisionRadius + b.collisionRadius
+        return (distance < sumOfRadii)
     }
     init() {
         // for(let i = 0; i < this.numberOfObstacles; i++) {
